9130f6e1eea4a534b2e307baaabc8cd27c22fbb7
moshikonachmias <moshiko.nachmias@gong.io>
2020-04-13 08:15:42 +0300

    sass

--- gong-exercises/src/components/shared/ButtonComponent.js
+++ gong-exercises/src/components/shared/ButtonComponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import '../../scss/mainT.css';
+import '../../scss/main.css';
 
 class ButtonComponent extends Component {
     render() {
